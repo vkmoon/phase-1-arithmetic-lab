6f9bef9316e29e1ac0936a5a0da294de6add1be1
Valeria <valeriakirkbride@gmail.com>
2023-08-28 23:23:30 -0600

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 5);
+
+let num3 = 24;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
